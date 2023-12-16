43b091b968d8ae41793b194c956c8f9b55278ec8
nguyed99 <nguyed99@zedat.fu-berlin.de>
2023-12-16 21:30:35 +0100

    Clean up code ...

---
+++ qcp.py
@@ -0,0 +1,13 @@
+"""
+This module simulates the 1D Quantum Contact Process
+(see https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.123.100604)
+using TeNPy (see https://github.com/tenpy/tenpy)
+"""
+
+import logging
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
---
+++ src/algorithms/wfmc.py
@@ -0,0 +1,21 @@
+"""
+This module contains an implementation of the Monte Carlo wave-function
+method.
+
+References:
+- Klaus Mølmer, Yvan Castin, and Jean Dalibard,
+  "Monte Carlo wave-function method in quantum optics,"
+  J. Opt. Soc. Am. B 10, 524-538 (1993)
+
+Implemented by: Robin Boulan
+"""
+
+import logging
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+def wave_function_monte_carlo(): 
+    pass
