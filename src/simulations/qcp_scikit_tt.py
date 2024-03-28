03ea159ed4af84e16a65a668cd4c323132b1c199
nguyed99 <nguyed99@zedat.fu-berlin.de>
2024-03-28 12:25:48 +0100

    update simulations with scikit_tt

--- src/simulations/qcp_scikit_tt.py
+++ src/simulations/qcp_scikit_tt.py
@@ -1,10 +1,10 @@
 import numpy as np
 from scikit_tt.tensor_train import (TT)
 import scikit_tt.tensor_train as tt
-from scikit_tt.solvers.ode import hod
+from scikit_tt.solvers.evp import als
 
 # parameters
-L = 51
+L = 25
 GAMMA = 1
 OMEGA = 6
 step_size = 1  # How to choose this?
@@ -13,21 +13,22 @@ max_rank = 200  # Bond dimension?
 
 # arrays
 identity = np.eye(2)
-sigmax = np.array([[0, 1], [1, 0]])
+sigmax = 0.5 * np.array([[0, 1], [1, 0]])
 annihilation_op = np.array([[0, 1], [0, 0]])
 number_op = np.array([[0, 0], [0, 1]])
 
 # core components
-S = 1j * GAMMA * (np.kron(annihilation_op, annihilation_op.T) - 0.5 * (np.kron(number_op, number_op)))
-L_1 = OMEGA * np.kron(sigmax, identity)
-L_2 = OMEGA * np.kron(number_op, identity)
-L_3 = -OMEGA * np.kron(identity, sigmax)
-L_4 = -OMEGA * np.kron(identity, number_op)
+S = GAMMA * np.kron(annihilation_op,
+                    annihilation_op) - 0.5 * (np.kron(number_op, identity) + np.kron(identity, number_op))
+L_1 = np.kron(sigmax, identity)
+L_2 = np.kron(identity, sigmax)
+L_3 = np.kron(number_op, identity)
+L_4 = np.kron(identity, number_op)
 Id = np.eye(4)
-M_1 = np.kron(number_op, identity)
-M_2 = np.kron(sigmax, identity)
-M_3 = np.kron(identity, number_op)
-M_4 = np.kron(identity, sigmax)
+M_1 = -1j * OMEGA * np.kron(number_op, identity)
+M_2 = -1j * OMEGA * np.kron(identity, number_op)
+M_3 = -1j * OMEGA * np.kron(sigmax, identity)
+M_4 = -1j * OMEGA * np.kron(identity, sigmax)
 
 # construct core
 op_cores = [None] * L
@@ -60,9 +61,16 @@ op_cores[-1][4, :, :, 0] = M_4
 op_cores[-1][5, :, :, 0] = S
 op = TT(op_cores)
 
+# compute ground state
+psi = tt.uniform(L * [4], 6)
+# eigenvalues, eigentensors, _ = als(op, psi, number_ev = 1, repeats = 10, sigma=0)
+eigenvalues, eigentensors, _ = als(op.transpose(conjugate=True) @ op, psi, number_ev=1, repeats=10, sigma=0)
+
+print(f"{eigenvalues=}")
+
 # simulate quantum contact process
-psi = tt.unit(L * [4], inds=25 * [0] + [3] + 25 * [0])
-solution = hod(op, psi, step_size=step_size, number_of_steps=number_of_steps, normalize=2, max_rank=max_rank)
+# psi = tt.unit(L * [4], inds=25 * [0] + [3] + 25 * [0])
+# solution = hod(op, psi, step_size=step_size, number_of_steps=number_of_steps, normalize=2, max_rank=max_rank)
 
 # compute active-site density
 # probs = np.zeros([len(solution), L])
