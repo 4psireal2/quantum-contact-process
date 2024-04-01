136de46a38054bc5c8ddacc5b13c75b58f8b4d9a
nguyed99 <nguyed99@zedat.fu-berlin.de>
2024-04-01 16:02:42 +0200

    More calculation for stationary states

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # quantum-contact-process-1D
-## TODO:
+## TODO (theory):
 - [] (*TenPy*) 2 time evolution algorithms: SingleSiteTDVP and TEBD (SVDBasedTEBDEngine + QRBasedTEBDEngine). Test both
 - [] (*TenPy*) WFMC implementation for [higher order](https://www.sciencedirect.com/science/article/pii/S0010465512000835?via%3Dihub)
 - [] (*TenPy*) Reproduce density plot in Hendrik's paper
@@ -9,6 +9,9 @@
 - [] (*theory*) Check whether the decay rates are smaller than the minium energy splitting in the system Hamiltonian -> approximations for the [validity](https://qutip.org/docs/latest/guide/dynamics/dynamics-master.html) of Lindblad Master equation
 - [] (*theory*) Check Jen's [approach](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.237201), LDPO?
 - [] (*technicality*) Putting code also at group's repo `itp0.physik.tu-berlin.de/home/agweimer`? (Check [wiki](https://www3.itp.tu-berlin.de/dokuwiki/agweimer:start))
+
+## TODO (simulations):
+- Correlation functions
 
 
 ## Technical details 
@@ -36,6 +39,12 @@ poetry install
 (# To update lock file based on poetry.lock
 poetry lock --no-update)
 ```
+
+### HPC-access
+```
+ssh  username@sheldon.physik.fu-berlin.de
+```
+
 
 #### *Bugs that are not mine ... So try at your own peril anything in []*
 - `conda-lock` somehow doesn't work for all platforms [`conda-lock -f environment.yml` or `conda-lock --lockfile conda-lock.yml`] (Something about mamba and conda that don't quite go well together ... see [issue](https://github.com/conda/conda-libmamba-solver/issues/418))
--- src/simulations/contact_process.py
+++ src/simulations/contact_process.py
@@ -1,12 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-from src.models.contact_process_model import (compute_site_expVal, construct_num_op, construct_basis_mps)
+import scikit_tt.tensor_train as tt
+from scikit_tt.solvers.evp import als
+
+from src.models.contact_process_model import (compute_site_expVal, construct_lindblad, construct_num_op)
 
 # path for results
 PATH = "/home/psireal42/study/quantum-contact-process-1D/results"
 
 # system parameters
-L = 10
+L = 50
 GAMMA = 1
 OMEGA = 6
 
@@ -18,34 +23,73 @@ step_size = 1  # How to choose this?
 number_of_steps = 10
 max_rank = 200  # Bond dimension?
 
-# compute spectral gap of L†L
-# Ls = list(range(10,35,5))
-# bond_dims = [8, 16, 32, 40, 50]
-# for bond_dim in bond_dims:
-#     # print(f"{L=}")
-#     print(f"{bond_dim=}")
-#     lindblad = construct_lindblad(gamma=GAMMA, omega=OMEGA, L=10)
-#     lindblad_hermitian = lindblad.transpose(conjugate=True) @ lindblad
+OMEGAS = np.linspace(0, 10, 20)
+bond_dims = np.array([10, 50, 100])
 
-#     psi = tt.uniform(10 * [4], ranks=bond_dim)
-#     time1 = time.time()
-#     eigenvalues, eigentensors, _ = als(lindblad_hermitian, psi, number_ev=1, repeats=10, conv_eps=1e-6, sigma=0)
-#     time2 = time.time()
-#     print(f"Elapsed time: {time2 - time1} seconds")
-#     print(f"Ground state energy per site E = {eigenvalues/10}")
-# assert abs(eigenvalues[0]) < 1e-5
+### Stationary state calculations
+spectral_gaps = []
+n_stag_s = np.zeros((bond_dims.shape[0], OMEGAS.shape[0]))
+n_s = np.zeros((bond_dims.shape[0], OMEGAS.shape[0]))
 
-# plt.figure()
-# plt.plot(eigenvalues, 'o', label=f"{L=}")
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig(PATH + f"/eigenvalues_spectrum_{L=}.png")
+for i, OMEGA in enumerate(OMEGAS):
+    for j, bond_dim in enumerate(bond_dims):
+        lindblad = construct_lindblad(gamma=GAMMA, omega=OMEGA, L=L)
+        lindblad_hermitian = lindblad.transpose(conjugate=True) @ lindblad
 
-# compute particle numbers
-basis_0 = np.array([1, 0])
-basis_1 = np.array([0, 1])
-psi = construct_basis_mps(L, basis=[np.kron(basis_1, basis_1)] * L)
-particle_nums = compute_site_expVal(psi, construct_num_op(L))
+        psi = tt.uniform(L * [4], ranks=bond_dim)
+        time1 = time.time()
+        eigenvalues, eigentensors, _ = als(lindblad_hermitian, psi, number_ev=2, repeats=10, conv_eps=1e-6, sigma=0)
+        time2 = time.time()
+        print(f"Elapsed time: {time2 - time1} seconds")
+        print(f"Ground state energy per site E = {eigenvalues/L}")
+        assert abs(eigenvalues[0]) < 1e-5
+
+        # compute spectral gap of L†L for biggest bond dimension
+        if bond_dim == bond_dims[-1]:
+            spectral_gaps.append(abs(eigenvalues[1] - eigenvalues[0]))
+
+        # compute staggered particle numbers
+        particle_nums = compute_site_expVal(eigentensors[0], construct_num_op(L))
+        n_s[j, i] = np.mean(particle_nums)
+
+        # compute staggered particle numbers
+        signs = np.array([-1 if i % 2 == 0 else 1 for i in range(L)])
+        stag_particle_nums = np.sqrt(np.mean(signs * particle_nums**2))
+        n_stag_s[j, i] = stag_particle_nums
+
+# plot spectral gap
+plt.figure()
+plt.plot(OMEGAS, spectral_gaps, 'o', label=f"{L=}")
+plt.legend()
+plt.tight_layout()
+plt.savefig(PATH + "/spectral_gaps.png")
+
+# plot particle numbers
+plt.figure()
+for i, bond_dim in enumerate(bond_dims):
+    plt.plot(OMEGAS, n_s[i, :], label=f"$\Chi=${bond_dim}")
+plt.xlabel(r"$\Omega$")
+plt.ylabel(r"$n_s$")
+plt.legend()
+plt.tight_layout()
+plt.savefig(PATH + "/stationary_density.png")
+
+# plot staggered particle numbers
+plt.figure()
+for i, bond_dim in enumerate(bond_dims):
+    plt.plot(OMEGAS, n_stag_s[i, :], label=f"$\Chi=${bond_dim}")
+plt.xlabel(r"$\Omega$")
+plt.ylabel(r"$n_{staggered\_s}$")
+plt.legend()
+plt.tight_layout()
+plt.savefig(PATH + "/staggered_stationary_density.png")
+
+# basis_0 = np.array([1, 0])
+# basis_1 = np.array([0, 1])
+# psi = construct_basis_mps(L, basis=[np.kron(basis_1, basis_1)] * L)
+# particle_nums = compute_site_expVal(psi, construct_num_op(L))
+
+# compute exponential decay
 
 # simulate quantum contact process
 # psi = tt.unit(L * [4], inds=25 * [0] + [3] + 25 * [0])
